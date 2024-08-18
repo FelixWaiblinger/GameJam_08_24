9865acb6324435c6833c7f3632672f116bd5adc9
Patrick <paddyblues@gmx.de>
2024-08-18 03:10:21 +0200

    VoidEventChannel

---
+++ Assets/Scripts/VoidEventChannel.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "VoidEventChannel", menuName = "Events/Void Event Channel")]
+public class VoidEventChannel : ScriptableObject {
+    public UnityAction OnVoidEventRaised;
+
+    public void RaiseVoidEvent() {
+        OnVoidEventRaised?.Invoke();
+    }
+}
