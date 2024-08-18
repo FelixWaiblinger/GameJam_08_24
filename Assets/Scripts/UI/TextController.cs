80724ced0c24e5525abde0d23c2144865d45ffa8
Patrick <paddyblues@gmx.de>
2024-08-18 03:45:10 +0200

    TextController and Button SOs Finished

--- Assets/Scripts/UI/TextController.cs
+++ Assets/Scripts/UI/TextController.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using TMPro;
 using UnityEngine;
 
-public class TextController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class TextController : MonoBehaviour {
+    [SerializeField] private TextMeshProUGUI _scalesCount;
+    [SerializeField] private TextMeshProUGUI _healthCount;
+
+    [SerializeField] private IntEventChannel _scalesCountEventChannel;
+    [SerializeField] private IntEventChannel _healthCountEventChannel;
+
+    private void OnEnable() {
+        _scalesCountEventChannel.OnIntEventRaised += ChangeScalesCount;
+        _healthCountEventChannel.OnIntEventRaised += ChangeHealthCount;
+
+    }
+    private void OnDisable() {
+        _scalesCountEventChannel.OnIntEventRaised -= ChangeScalesCount;
+        _healthCountEventChannel.OnIntEventRaised -= ChangeHealthCount;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void ChangeHealthCount(int arg0) {
+        _healthCount.text = "HP: " + arg0;
+    }
+
+    private void ChangeScalesCount(int arg0) {
+        _scalesCount.text = "Scales: " + arg0;
     }
 }
